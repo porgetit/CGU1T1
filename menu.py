eb9ba76a050a96083c81a52aec0e938dba4f0cc2
porgetit <kevin.esguerra@utp.edu.co>
2025-02-19 21:41:23 -0500

    19-02-2025

--- menu.py
+++ menu.py
@@ -3,12 +3,12 @@ import sys
 
 # Opciones del menú con las rutas de los archivos de prueba en cada carpeta
 PRUEBAS = {
-    "1": ("Prueba de Caída Libre", "Taller 1/caida_libre/pruebas.py"),
-    "2": ("Prueba de Cálculo de Desplazamiento", "Taller 1/calculo_desplazamiento/pruebas.py"),
-    "3": ("Prueba de Conversor de Velocidad", "Taller 1/conversor_velocidad/pruebas.py"),
-    "4": ("Prueba de Lanzamiento de Proyectiles", "Taller 1/lanzamiento_proyectiles/pruebas.py"),
-    "5": ("Prueba de Producto Escalar", "Taller 1/producto_escalar/pruebas.py"),
-    "6": ("Prueba de Suma de Vectores", "Taller 1/suma_vectores/pruebas.py"),
+    "1": ("Prueba de Caída Libre", "./caida_libre/pruebas.py"),
+    "2": ("Prueba de Cálculo de Desplazamiento", "./calculo_desplazamiento/pruebas.py"),
+    "3": ("Prueba de Conversor de Velocidad", "./conversor_velocidad/pruebas.py"),
+    "4": ("Prueba de Lanzamiento de Proyectiles", "./lanzamiento_proyectiles/pruebas.py"),
+    "5": ("Prueba de Producto Escalar", "./producto_escalar/pruebas.py"),
+    "6": ("Prueba de Suma de Vectores", "./suma_vectores/pruebas.py"),
     "0": ("Salir", None),
 }
 
